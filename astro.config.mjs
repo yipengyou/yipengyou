95153855d5461a0d9821339acdc11eb6c8b87e24
yipengyou <990722ansonchang@gmail.com>
2025-09-25 19:19:09 -0400

    routing issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://yipengyou.github.io",
+    site: "https://yipengyou.github.io/yipengyou/",
     base: "/",
 });
